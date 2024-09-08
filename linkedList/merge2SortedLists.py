9a3c8ca43adacab3f160769dee84f4704f724e49
kaitheengineer <93702715+baaska23@users.noreply.github.com>
2024-09-08 16:04:57 +0800

    my leetcode solutions

---
+++ linkedList/merge2SortedLists.py
@@ -0,0 +1,21 @@
+#Input: list1 = [1,2,4], list2 = [1,3,4]
+#Output: [1,1,2,3,4,4]
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        dummy = current = ListNode(0) # type: ignore
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next =l1
+                l1=l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        current.next =l1 or l2
+        return dummy.next
+
