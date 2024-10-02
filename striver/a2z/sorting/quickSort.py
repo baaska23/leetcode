f4a6fb1b30c1b8e1f34836c63f50e996a457ec44
kaitheengineer <93702715+baaska23@users.noreply.github.com>
2024-10-03 00:29:13 +0800

    push3

--- striver/a2z/sorting/quickSort.py
+++ striver/a2z/sorting/quickSort.py
@@ -1,12 +1,16 @@
 def partition(arr, low, high):
+    i = low
+    j = high-1
     pivot = arr[high]
-    i = low - 1
-    for j in range(low, high):
-        if arr[j] <= pivot:
-            i += 1
+    while i<j:
+        if arr[i] > pivot:
             arr[i], arr[j] = arr[j], arr[i]
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
+            j -= 1
+        else:
+            i += 1
+    if arr[i] > pivot:
+        arr[i], arr[high] = arr[high], arr[i]
+    return i
 
 def quickSort(arr, low, high):
     if low < high:
@@ -19,15 +23,15 @@ n = len(arr)
 quickSort(arr, 0, n - 1)
 print("Sorted array:", arr)
 
-def quickSort2(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quickSort(left) + middle + quickSort(right)
+# def quickSort2(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     pivot = arr[len(arr) // 2]
+#     left = [x for x in arr if x < pivot]
+#     middle = [x for x in arr if x == pivot]
+#     right = [x for x in arr if x > pivot]
+#     return quickSort(left) + middle + quickSort(right)
 
-arr = [10, 7, 8, 9, 1, 5]
-sorted_arr = quickSort2(arr)
-print("Sorted array:", sorted_arr)
+# arr = [10, 7, 8, 9, 1, 5]
+# sorted_arr = quickSort2(arr)
+# print("Sorted array:", sorted_arr)
---
+++ striver/a2z/sorting/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+def quickSort2(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     pivot = arr[len(arr) // 2]
+#     left = [x for x in arr if x < pivot]
+#     middle = [x for x in arr if x == pivot]
+#     right = [x for x in arr if x > pivot]
+#     return quickSort(left) + middle + quickSort(right)
+
+# arr = [10, 7, 8, 9, 1, 5]
+# sorted_arr = quickSort2(arr)
+# print("Sorted array:", sorted_arr)
